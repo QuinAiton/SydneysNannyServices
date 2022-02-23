73991e22a75d895688c75452f8b4c1fd612384c5
quinaiton <quinaiton@hotmail.com>
2022-02-23 12:32:31 -0800

    makes writing bigger and fixes logo

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,38 +1,23 @@
 import React from 'react';
 import Image from 'next/image';
-import logo from '../public/logo.jpeg';
+import logo2 from '../public/logo2.png';
 const Nav = () => {
   return (
-    <nav className='rounded border-gray-200 bg-white px-2 py-2.5 dark:bg-gray-800 sm:px-4'>
+    <nav className='  bg-[#fffbf5] px-2 py-2.5 sm:px-4'>
       <div className='container mx-auto flex flex-wrap items-center justify-between'>
-        <a href='#' className='flex'>
-          <svg
-            className='mr-3 h-10'
-            viewBox='0 0 52 72'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              d='M1.87695 53H28.7791C41.5357 53 51.877 42.7025 51.877 30H24.9748C12.2182 30 1.87695 40.2975 1.87695 53Z'
-              fill='#76A9FA'
-            ></path>
-            <path
-              d='M0.000409561 32.1646L0.000409561 66.4111C12.8618 66.4111 23.2881 55.9849 23.2881 43.1235L23.2881 8.87689C10.9966 8.98066 1.39567 19.5573 0.000409561 32.1646Z'
-              fill='#A4CAFE'
-            ></path>
-            <path
-              d='M50.877 5H23.9748C11.2182 5 0.876953 15.2975 0.876953 28H27.7791C40.5357 28 50.877 17.7025 50.877 5Z'
-              fill='#1C64F2'
-            ></path>
-          </svg>
-          <span className='self-center whitespace-nowrap text-lg font-semibold dark:text-white'>
-            SNS
-          </span>
-        </a>
+        <div>
+          <div className='flex justify-center'>
+            <Image src={logo2} height={40} width={40} />
+            <span className=' self-end whitespace-nowrap text-lg font-semibold text-black'>
+              SNS
+            </span>
+          </div>
+            <span className='text-sm'> Sydney's Nanny Service</span>
+        </div>
         <div className='flex md:order-2'>
           <button
             type='button'
-            className='mr-3 rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 md:mr-0'
+            className='focus:ring-blue-300md:mr-0 mr-3 rounded-lg bg-black px-8 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:ring-4'
           >
             Book Now!
           </button>
@@ -77,34 +62,25 @@ const Nav = () => {
           <ul className='mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium'>
             <li>
               <a
-                href='#'
-                className='block rounded bg-blue-700 py-2 pr-4 pl-3 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700'
+                href='#landing'
+                className=' text-lg text-gray-500 hover:text-black'
                 aria-current='page'
               >
                 Home
               </a>
             </li>
             <li>
-              <a
-                href='#'
-                className='block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white'
-              >
+              <a href='#about' className=' text-lg text-gray-500 hover:text-black'>
                 About Us
               </a>
             </li>
             <li>
-              <a
-                href='#'
-                className='block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white'
-              >
+              <a href='#services' className=' text-lg text-gray-500 hover:text-black'>
                 Services
               </a>
             </li>
             <li>
-              <a
-                href='#'
-                className='block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white'
-              >
+              <a href='#contact' className=' text-lg text-gray-500 hover:text-black'>
                 Contact
               </a>
             </li>
