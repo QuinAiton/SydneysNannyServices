34985424060d219cbfb4fe0d20f0dd3d168a4e89
quinaiton <quinaiton@hotmail.com>
2022-02-23 12:33:02 -0800

    adds service component

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,11 +2,13 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Nav from '../components/Nav.jsx';
 import JumboTron from '../components/JumboTron.jsx';
+import Services from '../components/Services'
 const index = () => {
   return (
     <div>
       <Nav />
-      <JumboTron />
+      <JumboTron /> 
+      <Services/>
     </div>
   );
 };
