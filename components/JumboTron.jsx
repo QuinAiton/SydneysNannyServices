ebe79bc4065c526d98d7804235a739582efda36d
quinaiton <quinaiton@hotmail.com>
2022-02-26 15:21:27 -0800

    adds white text box and updates colors

--- components/JumboTron.jsx
+++ components/JumboTron.jsx
@@ -12,23 +12,24 @@ const JumboTron = () => {
           src={childpaintimage}
           alt={'child painting'}
         />
-        <h2 className=' relative pt-[25vh] text-center text-2xl font-bold leading-9 tracking-tight text-white sm:text-2xl sm:leading-10'>
-          Childcare you can trust
+        <h2 className=' relative pt-[25vh] text-center text-2xl font-bold leading-9 tracking-tight text-black sm:text-2xl sm:leading-10'>
+          Childcare you can trust 
         </h2>
         <div className='justify-cente relative flex justify-center pt-5'>
           <div className='inline-flex rounded-md bg-white shadow'>
             <a
               href='#'
-              className='py-2 px-8 font-bold text-gray-700 hover:rounded hover:bg-gray-700 hover:text-white'
+              className='py-2 px-8 font-bold text-gray-700 hover:rounded hover:bg-pink-400 hover:text-white'
             >
               Services
             </a>
           </div>
         </div>
       </div>
-      <div className='bg-white] h-[20vh]'>
-        <h2 className='pt-20 text-center text-xl'>
-          We are a childcare buisness located in Pemberton BC
+      <div className='bg-white h-auto w-[70vw] mx-auto mt-20 rounded-xl shadow-xl p-10 '>
+        <h2 className='text-center text-xl'>
+          We are a childcare buisness located in Pemberton BC <br/>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem sunt, optio aperiam nihil aut ipsa eaque esse unde perferendis suscipit quia commodi harum ab rerum necessitatibus corrupti dolores, animi obcaecati?
         </h2>
       </div>
     </div>
