3a58004b23a0f9e794e9d5c2376da6a73d05935f
quinaiton <quinaiton@hotmail.com>
2022-02-26 15:17:23 -0800

    Adds contact form and makes small style changes to sizing

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -3,15 +3,16 @@ import React from 'react';
 const Contact = () => {
   return (
     <div
-      className='flex min-h-screen w-full items-center justify-center'
+      className='flex flex-col min-h-screen w-full items-center justify-center shadow-2xl'
       id='contact'
     >
-      <div className='relative flex w-3/4 flex-col justify-around space-y-10  rounded-xl overflow-hidden bg-[#fef8e4] p-10 text-white shadow-lg md:flex-row md:space-x-1 md:space-y-0 sm:w-[90vw]'>
+      <div className='relative flex w-[90vw] flex-col justify-around space-y-10  overflow-hidden rounded-xl bg-[#fef8e4] p-10 text-white shadow-lg sm:w-[90vw] md:flex-row md:space-x-1 md:space-y-0'>
         <div className='absolute -left-12 bottom-14 z-0 h-28 w-28 rounded-full bg-white'></div>
         <div className='relative z-10 flex flex-col justify-between space-y-8'>
           <div>
-            <h1 className='bg-gradient-to-r from-cyan-400 to-teal-400 bg-clip-text text-4xl font-bold tracking-wide text-transparent'>
+            <h1 className='bg-gradient-to-r from-pink-400 to-teal-400 bg-clip-text text-3xl font-bold tracking-wide text-transparent'>
               Contact Information
+              <div className='mx-auto h-1 w-[10vw] rounded bg-indigo-200 opacity-75' />
             </h1>
           </div>
           <div className='flex flex-col space-y-6'>
@@ -97,14 +98,15 @@ const Contact = () => {
         <div className='relative w-auto'>
           <div className='absolute -right-24 -bottom-24 z-0 h-40 w-60 rounded-full bg-white'></div>
           <div className='absolute -left-32 -top-20 z-0 h-44 w-44 rounded-full bg-white'></div>
-          <div className='relative z-10 rounded-xl bg-white p-8 text-gray-600 shadow-xl md:w-150'>
-            <form action='' className='flex flex-col space-y-6 py-3 w-auto '>
+          <div className='relative z-10 rounded-xl bg-white p-8 text-gray-600 shadow-xl md:w-[40vw]'>
+            <form action='' className='flex w-auto flex-col space-y-6 py-3 '>
               <div className='relative  rounded-md border-[2px] outline-gray-500'>
                 <input
                   type='text'
                   name='name'
                   placeholder='Full Name '
                   className='block w-full appearance-none rounded-md border-none bg-transparent p-2 shadow-inner focus-within:border-teal-400  focus:outline-none'
+                  required
                 />
               </div>
               <div className='relative  rounded-md border-[2px] outline-gray-500'>
@@ -113,6 +115,7 @@ const Contact = () => {
                   name='email'
                   placeholder=' Email'
                   className='block w-full appearance-none rounded-md bg-transparent p-2 shadow-inner focus-within:border-teal-400  focus:outline-none'
+                  required
                 />
               </div>
               <div className='relative  rounded-md border-[2px] outline-gray-500 '>
@@ -121,15 +124,17 @@ const Contact = () => {
                   rows='6'
                   placeholder=' Message'
                   className='block w-full resize-none appearance-none rounded-md border-none bg-transparent p-2 shadow-inner focus-within:border-teal-400 focus:outline-none'
+                  required
                 ></textarea>
               </div>
-              <button className='inline-block self-end rounded-lg bg-cyan-700 px-6 py-2 text-sm font-bold uppercase text-white ring-cyan-700 ring-offset-2 transition duration-300 ease-in-out hover:bg-cyan-600 focus:outline-none focus:ring-2'>
+              <button className='inline-block self-end rounded-lg bg-cyan-700 px-6 py-2 text-sm font-bold uppercase text-white ring-cyan-700 ring-offset-2 transition duration-300 ease-in-out hover:bg-pink-400 focus:outline-none focus:ring-2'>
                 Send Message
               </button>
             </form>
           </div>
         </div>
-      </div>
+      </div> 
+      <h2 className='mt-[10vh] text-2xl'> I  look forward to hearing from you!</h2>
     </div>
   );
 };
